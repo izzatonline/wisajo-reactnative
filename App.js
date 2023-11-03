d586d258f714d241e2c8717e5d59fe8756e96b31
izzatonline <izzat.online@gmail.com>
2023-11-03 18:21:29 +0800

    added WelcomeScreen and ViewImageScreen

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Hello Izzat</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
