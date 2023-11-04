b6b1a1ced83442e13759820968fa3d19bf767611
izzatonline <izzat.online@gmail.com>
2023-11-05 01:57:29 +0800

    updated screens

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
+import AppText from "./app/components/AppText";
+
 export default function App() {
-    return <WelcomeScreen />;
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <AppText
+                style={{
+                    fontFamily: "Courier New",
+                    fontSize: 30,
+                }}
+            >
+                I love React Native
+            </AppText>
+        </View>
+    );
 }
